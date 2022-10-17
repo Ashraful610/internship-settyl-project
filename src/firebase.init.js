e5ce99486db943a5d75c00a477378fd30cf8b7bf
Ashraful610 <ashalam610@gmail.com>
2022-10-17 10:47:45 +0600

    connect with git hub

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'react-firebase-hooks'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCDHPonUv2SY4qD77-lnzjAR2D0VAMSf4Q",
+  authDomain: "internship-settyl-project.firebaseapp.com",
+  projectId: "internship-settyl-project",
+  storageBucket: "internship-settyl-project.appspot.com",
+  messagingSenderId: "450347106972",
+  appId: "1:450347106972:web:802aa57325a1586d71e359"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default auth
